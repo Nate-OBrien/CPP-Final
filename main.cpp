e8ecd45ef783aecd9c9ad75965bdb8b5184f4ca8
JakeS112 <jake.sweat@mcvts.org>
2025-06-03 09:10:16 -0400

    dealer

--- main.cpp
+++ main.cpp
@@ -2,7 +2,40 @@
 
 using namespace std;
 
+bool win_lose(int dealer, int  player){
+    bool win;
+    if (((dealer < player)&&(player <= 21))|| (player <= 21) && (dealer > 21)){
+        win = true;
+    }
+    else{
+        win = false;
+    }
+    return win;
+}
+
+bool deal (int delaer){
+    bool deal;
+    if (dealer < 17){
+        deal = true;
+    }
+    else{
+        deal = false;
+    }
+    return deal;
+}
+int winning (bool win, int bet, int bank){
+    if win = true{
+        bank = bank + (bet*2);
+    }
+    else{
+        bank = bank;
+    }
+    return bank;
+}
+
+
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+   
+
+
 }
